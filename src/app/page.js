3a7018495a7bf0d5da21e1f89140d4a1c89d2dd0
Emmanuel <emanuelanyigor@gmail.com>
2025-01-25 05:24:45 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import './globals.css';
+import { socket } from '../../socket';
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const socket = io("http://localhost:4000");
+  const [message, setMessage] = useState("");
+  const [handle, setHandle] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleSubmit  = () => {
+    socket.emit("chat", {
+      message,
+      handle
+    });
+
+    setMessage("");
+  }
+
+  useEffect(() => {
+    function getChats(data){
+      console.log(data);
+      document.getElementById("output").innerHTML += `
+        <p><strong>${data.handle}:</strong> ${data.message}</p>
+      `;
+    }
+
+    socket.on('chat', getChats);
+
+    return () => {
+      socket.off('chat', getChats);
+    };
+  }, []);
+
+  return (
+    <div className="">
+      <div id='chat-window'>
+        <div id='output'></div>
+
+      </div>
+      <input 
+        id="handle" 
+        type="text" 
+        placeholder="Handle" 
+        value={handle} 
+        onChange={(e)=> setHandle(e.target.value)} 
+      />
+      <input 
+        id="message" 
+        type="text" 
+        placeholder="Message" 
+        value={message} 
+        onChange={(e)=> setMessage(e.target.value)}
+      />
+      <button id="send" onClick={handleSubmit}>Send</button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+    extend: {
+        colors: {
+            primary: '#FF7622',
+            primary_light: '#FFF1F2',
+            secondary: '#98A8B8',
+            dark: '#121223',
+            white: '#fff',
+            error: '#FF218C',
+            success: '#4bb543',
+        },
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '991px',
+            xl: '1280px'
+        },
+    }
+},
+plugins: [],
+};
