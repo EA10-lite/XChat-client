e766cb8d0d2f8ccee980ec0d0838e078d741d1e2
Emmanuel <emanuelanyigor@gmail.com>
2025-01-25 21:48:19 +0100

    changing icon when user is typinh

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { PiMicrophoneFill } from "react-icons/pi";
+import { MdSend } from "react-icons/md";
 import { EmojiMenu } from "../popovers";
 
 
 const ChatInput = () => {
+    const [message, setMessage] = useState("");
+
     return (
         <div className="footer absolute bottom-0 bg-white w-full min-h-[fit-content]">
             <div className="w-full border-t border-grey py-[12px] px-[24px] shadow-md">
@@ -14,10 +17,16 @@ const ChatInput = () => {
                             type="text" name="message" 
                             className="bg-[#f2f2f8] w-full text-sm py-[10px] px-[16px] rounded-[8px]"
                             placeholder="Send message to this user.."
+                            value={message}
+                            onChange={(e)=> setMessage(e.target.value)}
                         />
                     </div>
 
-                    <PiMicrophoneFill className="text-[24px] cursor-pointer" />
+                    { message?.length > 0 ? (
+                        <MdSend className="text-[24px] cursor-pointer" />
+                    ) : (
+                        <PiMicrophoneFill className="text-[24px] cursor-pointer" />
+                    )}
                 </div>
             </div>
         </div>
